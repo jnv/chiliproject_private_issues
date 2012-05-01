a820faeaaad289a61c75c626db49e1135fded494
Jan Vlnas <git@jan.vlnas.cz>
2012-05-01 17:51:27 +0200

    Show indicator in issue header

--- lib/private_issues/hook.rb
+++ lib/private_issues/hook.rb
@@ -3,5 +3,7 @@ module PrivateIssues
   class Hook < Redmine::Hook::ViewListener
     render_on :view_issues_form_details_bottom,
               :partial => 'hooks/private_issues/view_issues_form_details_bottom'
+    render_on :view_layouts_base_html_head, :partial => 'hooks/private_issues/html_head'
+    render_on :view_layouts_base_body_bottom, :partial => 'hooks/private_issues/body_bottom'
   end
 end
