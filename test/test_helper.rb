50c7d4b62c17c4f25df8ffd93822605b9e0b17f2
Jan Vlnas <git@jan.vlnas.cz>
2013-06-16 20:32:31 +0200

    Hack around missing Mocha constant
    See https://github.com/freerange/mocha/issues/110

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,7 @@
 # Load the normal Rails helper
 require File.expand_path('../../../../test/test_helper', File.dirname(__FILE__))
 
+Mocha::Integration::TestUnit::AssertionCounter = Mocha::Integration::AssertionCounter
+
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
