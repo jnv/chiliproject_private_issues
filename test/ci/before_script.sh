4d8d3239296c840cba8181b9e3713b77a2a3c747
Jan Vlnas <git@jan.vlnas.cz>
2013-06-16 20:04:29 +0200

    Travis build: Downgrade gem to 1.8, add more output

--- test/ci/before_script.sh
+++ test/ci/before_script.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
+echo "=== pwd"
 pwd
+
+echo "=== TEMPORARY HACK: downgrade gems due to Gem.source_index"
+gem update --system 1.8.25
 
 # Prepare ChiliProject
 if [ "x$MAIN_ARCHIVE" != "x" ]; then
+  echo "=== Downloading main project as tarball"
   wget $MAIN_ARCHIVE
   mkdir -p $TARGET_DIR
   tar -xf *.tar.gz --strip 1 -C $TARGET_DIR
 else
+  echo "=== Downloading main project as shallow clone"
   git clone --depth=100 $MAIN_REPO $TARGET_DIR
   #git submodule update --init --recursive
 fi
 cd $TARGET_DIR
 
 # Copy over the already downloaded plugin
+echo "=== Copying over plugin"
 cp -r ~/build/*/$REPO_NAME $TARGET_DIR/vendor/plugins/$PLUGIN_DIR
 
 #export BUNDLE_GEMFILE=$TARGET_DIR/Gemfile
 
 ruby --version
 
+echo "=== Running main project's bundle"
 bundle install --without=$BUNDLE_WITHOUT
 
-echo "creating $DB database"
+echo "=== Creating $DB database"
 case $DB in
   "mysql" )
     mysql -e 'create database chiliproject_test;'
@@ -54,5 +62,7 @@ EOF
     ;;
 esac
 
+echo "=== Migrating main project"
 bundle exec rake db:migrate
+echo "=== Migrating plugins"
 bundle exec rake db:migrate:plugins
